734fc7342a272a53c5fc151310a214ac5023ad32
Viktoria157 <epik190119@gmail.com>
2025-11-02 20:46:59 +0200

    Add files via upload

---
+++ laba7zav2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int month;
+    cout << "Введіть номер місяця (1–12): ";
+    cin >> month;
+
+    switch (month) {
+    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+        cout << "У цьому місяці 31 день." << endl;
+        break;
+    case 4: case 6: case 9: case 11:
+        cout << "У цьому місяці 30 днів." << endl;
+        break;
+    case 2:
+        cout << "У лютому 28 або 29 днів (залежно від року)." << endl;
+        break;
+    default:
+        cout << "Помилка! Номер місяця повинен бути від 1 до 12." << endl;
+    }
+
+    return 0;
+}
